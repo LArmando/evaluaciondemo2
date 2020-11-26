ad7fe02082616945c9527b360eb2cc7a19808976
Luis Armando <luis.armando.telles.castro@uabc.edu.mx>
2020-11-26 08:46:16 -0800

    Controlador Entidad Service

--- src/main/java/com/uabc/edu/evaluacionapp/controller/ProfesorController.java
+++ src/main/java/com/uabc/edu/evaluacionapp/controller/ProfesorController.java
@@ -5,8 +5,12 @@ import com.uabc.edu.evaluacionapp.service.impl.ProfesorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/profesores")
@@ -20,5 +24,49 @@ public class ProfesorController {
         return "profesores/registrar";
     }
 
+    @PostMapping(value = "/registrar")
+    public String registrarProfesor(@ModelAttribute Profesor profesor, RedirectAttributes redirectAttrs) {
 
+        service.registrarProfesor(profesor);
+        redirectAttrs.addFlashAttribute("mensaje", "Agregado correctamente");
+
+        return "redirect:/profesores/mostrar";
+    }
+
+    @GetMapping(value = "/mostrar")
+    public String mostrarProfesores(Model model) {
+        model.addAttribute("profesores", service.obtenerProfesores());
+        return "profesores/mostrar";
+    }
+
+    @GetMapping(value = "/eliminar/{id}")
+    public String eliminarProfesor(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("profesores", service.obtenerProfesores());
+
+        boolean test = service.eliminarProfesor(id);
+
+        return "redirect:/profesores/mostrar";
+    }
+
+    @PostMapping(value = "/modificar/{id}")
+    public String modificarProfesor(@PathVariable("id") Integer id, @ModelAttribute Profesor profesor, RedirectAttributes redirectAttrs, BindingResult result, Model model) {
+        /*if(result.hasErrors()){
+            profesor.setId(id);
+            return "/profesores/modificar";
+        }*/
+        model.addAttribute("profesores", service.obtenerProfesores());
+        service.modificarProfesor(profesor);
+        redirectAttrs.addFlashAttribute("mensaje", "Editado correctamente");
+
+        return "redirect:/profesores/mostrar";
+    }
+
+    @GetMapping(value = "/editar/{id}")
+    public String editarProfesor(@PathVariable("id") Optional<Integer> id, Model model) {
+        if (id.isPresent()) {
+            Profesor profesor = service.obtenerProfesor(id.get());
+            model.addAttribute("profesor", profesor);
+        }
+        return "/profesores/modificar";
+    }
 }
