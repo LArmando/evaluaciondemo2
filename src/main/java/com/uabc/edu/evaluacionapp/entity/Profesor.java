77f419212754fc55a4a11a1b13d0e56dc6a5b218
Luis Armando <luis.armando.telles.castro@uabc.edu.mx>
2020-11-26 21:50:24 -0800

    Controlador Entidad Service

--- src/main/java/com/uabc/edu/evaluacionapp/entity/Profesor.java
+++ src/main/java/com/uabc/edu/evaluacionapp/entity/Profesor.java
@@ -2,29 +2,24 @@ package com.uabc.edu.evaluacionapp.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
+@Table(name="profesor")
 public class Profesor {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id_profesor")
 
     private Integer id;
     private String nombre, apPaterno, apMaterno;
 
     public Profesor(){
-
     }
-
     public Profesor(String nombre, String apPaterno, String apMaterno) {
         this.nombre = nombre;
         this.apPaterno = apPaterno;
         this.apMaterno = apMaterno;
     }
-
-
 }
